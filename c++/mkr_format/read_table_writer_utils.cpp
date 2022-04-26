d073045eda9854b12a08ad9a738e21c7444c830c
George Pimm <george.pimm@nanoporetech.com>
2022-04-26 19:05:22 +0100

    remove null bitmaps

--- c++/mkr_format/read_table_writer_utils.cpp
+++ c++/mkr_format/read_table_writer_utils.cpp
@@ -15,20 +15,18 @@ template <typename Type>
 arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
         std::shared_ptr<arrow::DataType> const& type,
         PrimitiveDictionaryKeyBuilder<Type> const& builder,
-        std::shared_ptr<arrow::Buffer> const& null_bitmap,
         std::size_t expected_length) {
     arrow::TypedBufferBuilder<Type> buffer_builder;
     auto data_in = builder.get_data();
     ARROW_RETURN_NOT_OK(buffer_builder.Append(data_in.data(), data_in.size()));
     ARROW_ASSIGN_OR_RAISE(auto data, buffer_builder.FinishWithLength(expected_length));
 
-    return arrow::ArrayData::Make(type, expected_length, {null_bitmap, data}, 0);
+    return arrow::ArrayData::Make(type, expected_length, {nullptr, data}, 0);
 }
 
 arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
         std::shared_ptr<arrow::DataType> const& type,
         StringDictionaryKeyBuilder const& builder,
-        std::shared_ptr<arrow::Buffer> const& null_bitmap,
         std::size_t expected_length) {
     arrow::TypedBufferBuilder<std::uint8_t> value_builder;
     auto const& str_data = builder.get_string_data();
@@ -47,13 +45,12 @@ arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
     ARROW_RETURN_NOT_OK(offset_builder.Finish(&offsets));
     ARROW_RETURN_NOT_OK(value_builder.Finish(&value_data));
 
-    return arrow::ArrayData::Make(type, expected_length, {null_bitmap, offsets, value_data}, 0, 0);
+    return arrow::ArrayData::Make(type, expected_length, {nullptr, offsets, value_data}, 0, 0);
 }
 
 arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
         std::shared_ptr<arrow::DataType> const& type,
         StringMapDictionaryKeyBuilder const& builder,
-        std::shared_ptr<arrow::Buffer> const& null_bitmap,
         std::size_t expected_length) {
     arrow::TypedBufferBuilder<std::int32_t> offset_builder;
     auto const& offset_data = builder.get_offset_data();
@@ -77,19 +74,16 @@ arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
     auto const& key_type = map_type->key_type();
     auto const& item_type = map_type->item_type();
 
-    std::shared_ptr<arrow::Buffer> map_item_null_bitmap;
-    ARROW_ASSIGN_OR_RAISE(auto key_data, get_array_data(key_type, builder.key_builder(),
-                                                        map_item_null_bitmap, final_item_length));
-    ARROW_ASSIGN_OR_RAISE(auto item_data, get_array_data(item_type, builder.value_builder(),
-                                                         map_item_null_bitmap, final_item_length));
+    ARROW_ASSIGN_OR_RAISE(auto key_data, get_array_data(key_type, builder.key_builder(), final_item_length));
+    ARROW_ASSIGN_OR_RAISE(auto item_data, get_array_data(item_type, builder.value_builder(), final_item_length));
 
     // Pack this data out as a struct:
     std::shared_ptr<arrow::ArrayData> items =
             arrow::ArrayData::Make(map_type->value_type(), final_item_length,
-                                   {null_bitmap, offsets}, {key_data, item_data}, 0);
+                                   {nullptr, offsets}, {key_data, item_data}, 0);
 
     // And add this struct to the map/list as the value data, along with the offsets:
-    auto data = arrow::ArrayData::Make(type, expected_length, {null_bitmap, offsets},
+    auto data = arrow::ArrayData::Make(type, expected_length, {nullptr, offsets},
                                        {std::move(items)}, 0);
 
     arrow::MapArray array(data);
@@ -103,7 +97,6 @@ arrow::Result<std::shared_ptr<arrow::ArrayData>> get_array_data(
 template <std::size_t CurrentIndex, typename BuilderTuple>
 arrow::Status do_struct_array_data_unpack(std::vector<std::shared_ptr<arrow::ArrayData>>& dest,
                                           std::shared_ptr<arrow::StructType> const& type,
-                                          std::shared_ptr<arrow::Buffer> const& null_bitmap,
                                           std::size_t expected_length,
                                           BuilderTuple const& builders) {
     static_assert(CurrentIndex >= 0, "Current index must be greater than zero");
@@ -112,7 +105,7 @@ arrow::Status do_struct_array_data_unpack(std::vector<std::shared_ptr<arrow::Arr
     auto const& builder = std::get<CurrentIndex>(builders);
 
     ARROW_ASSIGN_OR_RAISE(dest[CurrentIndex],
-                          get_array_data(field_type, builder, null_bitmap, expected_length));
+                          get_array_data(field_type, builder, expected_length));
     return Status::OK();
 }
 
@@ -123,11 +116,10 @@ template <typename BuilderTuple>
 struct UnpackStructArrayData<0, BuilderTuple> {
     static arrow::Status unpack(std::vector<std::shared_ptr<arrow::ArrayData>>& dest,
                                 std::shared_ptr<arrow::StructType> const& type,
-                                std::shared_ptr<arrow::Buffer> const& null_bitmap,
                                 std::size_t expected_length,
                                 BuilderTuple const& builders) {
         // Dump the last item:
-        return do_struct_array_data_unpack<0>(dest, type, null_bitmap, expected_length, builders);
+        return do_struct_array_data_unpack<0>(dest, type, expected_length, builders);
     }
 };
 
@@ -135,15 +127,14 @@ template <std::size_t CurrentIndex, typename BuilderTuple>
 struct UnpackStructArrayData {
     static arrow::Status unpack(std::vector<std::shared_ptr<arrow::ArrayData>>& dest,
                                 std::shared_ptr<arrow::StructType> const& type,
-                                std::shared_ptr<arrow::Buffer> const& null_bitmap,
                                 std::size_t expected_length,
                                 BuilderTuple const& builders) {
         // Dump this builders first:
-        RETURN_NOT_OK(do_struct_array_data_unpack<CurrentIndex>(dest, type, null_bitmap,
+        RETURN_NOT_OK(do_struct_array_data_unpack<CurrentIndex>(dest, type,
                                                                 expected_length, builders));
         // Then recursively dump the other builders:
         return UnpackStructArrayData<CurrentIndex - 1, BuilderTuple>::unpack(
-                dest, type, null_bitmap, expected_length, builders);
+                dest, type, expected_length, builders);
     }
 };
 
@@ -153,12 +144,7 @@ arrow::Result<std::shared_ptr<arrow::StructArray>> get_struct_array(
         BuilderTuple const& builders) {
     auto const length = std::get<0>(builders).length();
 
-    // Build null bitmap:
-    arrow::TypedBufferBuilder<bool> null_bitmap_builder;
-    ARROW_RETURN_NOT_OK(null_bitmap_builder.Append(length, true));
     std::int64_t null_count = 0;
-    std::shared_ptr<arrow::Buffer> null_bitmap;
-    RETURN_NOT_OK(null_bitmap_builder.Finish(&null_bitmap));
 
     if (type->num_fields() != std::tuple_size<BuilderTuple>::value) {
         return Status::Invalid("Invalid builder count for number of struct fields");
@@ -167,10 +153,10 @@ arrow::Result<std::shared_ptr<arrow::StructArray>> get_struct_array(
     // Extract child data:
     std::vector<std::shared_ptr<arrow::ArrayData>> child_data(std::tuple_size<BuilderTuple>::value);
     using Unpacker = UnpackStructArrayData<std::tuple_size<BuilderTuple>::value - 1, BuilderTuple>;
-    RETURN_NOT_OK(Unpacker::unpack(child_data, type, null_bitmap, length, builders));
+    RETURN_NOT_OK(Unpacker::unpack(child_data, type, length, builders));
 
     // Compile into array data:
-    auto array = arrow::ArrayData::Make(type, length, {null_bitmap}, null_count);
+    auto array = arrow::ArrayData::Make(type, length, {nullptr}, null_count);
     array->child_data = std::move(child_data);
     return std::make_shared<arrow::StructArray>(array);
 }
