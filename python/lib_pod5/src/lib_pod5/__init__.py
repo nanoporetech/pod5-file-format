bf50417edd2277ca0672457c214c203c9222bb19
Richard Harris <richard.harris@nanoporetech.com>
2022-11-25 11:37:50 +0000

    explicit export in lib-pod5.__init__.py

--- python/lib_pod5/src/lib_pod5/__init__.py
+++ python/lib_pod5/src/lib_pod5/__init__.py
@@ -1,3 +1,21 @@
 """ Imports everything from the pod5_format_pybind11 module and associated .pyi"""
 
-from .pod5_format_pybind import *
+from .pod5_format_pybind import (
+    EmbeddedFileData,
+    FileWriter,
+    FileWriterOptions,
+    Pod5AsyncSignalLoader,
+    Pod5FileReader,
+    Pod5RepackerOutput,
+    Pod5SignalCacheBatch,
+    Repacker,
+    compress_signal,
+    create_file,
+    decompress_signal,
+    format_read_id_to_str,
+    get_error_string,
+    load_read_id_iterable,
+    open_file,
+    update_file,
+    vbz_compressed_signal_max_size,
+)
