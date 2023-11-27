a6b9c9b984d60f16aff70c60568c48d0de9a711b
George Pimm <george.pimm@nanoporetech.com>
2023-11-27 11:01:20 +0000

    Add python 3.12 support

--- ci/setup_python_osx.sh
+++ ci/setup_python_osx.sh
@@ -15,7 +15,7 @@ git clone https://github.com/gregneagle/relocatable-python.git
 echo "Generating python ${version} into ${destination}"
 
 os_version="10.9"
-if [[ "${version}" == "3.10.10" || "${version}" == "3.11.2" ]]; then
+if [[ "${version}" == "3.10.10" || "${version}" == "3.11.2" || "${version}" == "3.12.0" ]]; then
     os_version="11"
 fi
 
