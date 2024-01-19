131b779ac2f43ec22100cb84258ad96b0fa12510
Michael Xymitoulias <michael.xymitoulias@nanoporetech.com>
2024-01-19 12:53:24 +0000

    Purge GCC7

--- ci/install.sh
+++ ci/install.sh
@@ -8,17 +8,14 @@ set -o nounset
 # Tar up the archive build:
 (
     cmake -DCMAKE_INSTALL_PREFIX="archive" -DBUILD_TYPE="Release" -DCOMPONENT="archive" -P "cmake_install.cmake"
-    if [[ "$OSTYPE" = "linux-gnu"* ]] && [ "$#" -ge 1 ] && [ "$1" = "STATIC_BUILD" ]; then
-        gcc_version=$(gcc --version | awk 'NR==1 {print $3}' | sed 's/\([0-9.]*\).*/\1/')
-        target_dir="archive/lib"
-
-        [ "$gcc_version" != "7.5.0" ] && target_dir="archive/lib64"
-        cp "../build/third_party/libs"/* "$target_dir"
-    else
-        if [ "$#" -ge 1 ] && [ "$1" = "STATIC_BUILD" ]; then
+    if [ "$#" -ge 1 ] && [ "$1" == "STATIC_BUILD" ]; then
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            cp "../build/third_party/libs"/* "archive/lib64"
+        else
             cp "../build/third_party/libs"/* "archive/lib"
         fi
     fi
+
 
 )
 
