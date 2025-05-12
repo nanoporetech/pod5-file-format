cbd8dd8c5bbff010753ecd3339e324edde3445db
Ben Lawrence <ben.lawrence@nanoporetech.com>
2025-05-12 17:22:31 +0100

    [INSTX-9827] Remove dead code
    The approach was dropped previously but the code was sitting around
    making the coverage look bad.

--- c++/pod5_format/read_table_writer_utils.h
+++ c++/pod5_format/read_table_writer_utils.h
@@ -20,46 +20,6 @@ namespace pod5 {
 
 namespace detail {
 
-template <typename T>
-class PrimitiveDictionaryKeyBuilder {
-public:
-    arrow::Status init_buffer(arrow::MemoryPool * pool) { return m_values.init_buffer(pool); }
-
-    arrow::Status append(T const & value) { return m_values.append(value); }
-
-    std::size_t length() const { return m_values.size(); }
-
-    std::shared_ptr<arrow::Buffer> get_data() const { return m_values.get_buffer(); }
-
-private:
-    ExpandableBuffer<T> m_values;
-};
-
-// Working around std::vector<bool> and arrow bool types not playing well together.
-template <>
-class PrimitiveDictionaryKeyBuilder<bool> {
-public:
-    arrow::Status init_buffer(arrow::MemoryPool * pool) { return m_values.init_buffer(pool); }
-
-    arrow::Status append(bool value)
-    {
-        ARROW_RETURN_NOT_OK(m_values.resize((m_bit_length / 8) + 1));
-        auto mutable_data = m_values.mutable_data();
-        arrow::bit_util::SetBitTo(mutable_data, m_bit_length, value);
-        m_bit_length += 1;
-
-        return arrow::Status::OK();
-    }
-
-    std::size_t length() const { return m_bit_length; }
-
-    std::shared_ptr<arrow::Buffer> get_data() const { return m_values.get_buffer(); }
-
-private:
-    ExpandableBuffer<std::uint8_t> m_values;
-    std::size_t m_bit_length = 0;
-};
-
 class StringDictionaryKeyBuilder {
 public:
     StringDictionaryKeyBuilder(arrow::MemoryPool * pool = nullptr)
@@ -95,81 +55,7 @@ private:
     ExpandableBuffer<std::uint8_t> m_string_values;
 };
 
-class StringMapDictionaryKeyBuilder {
-public:
-    arrow::Status init_buffer(arrow::MemoryPool * pool)
-    {
-        ARROW_RETURN_NOT_OK(m_offset_values.init_buffer(pool));
-        ARROW_RETURN_NOT_OK(m_key_builder.init_buffer(pool));
-        return m_value_builder.init_buffer(pool);
-    }
-
-    arrow::Status append(pod5::RunInfoData::MapType const & value)
-    {
-        ARROW_RETURN_NOT_OK(m_offset_values.append(m_key_builder.length()));
-        for (auto const & item : value) {
-            ARROW_RETURN_NOT_OK(m_key_builder.append(item.first));
-            ARROW_RETURN_NOT_OK(m_value_builder.append(item.second));
-        }
-        return arrow::Status::OK();
-    }
-
-    std::size_t length() const { return m_offset_values.size(); }
-
-    StringDictionaryKeyBuilder const & key_builder() const { return m_key_builder; }
-
-    StringDictionaryKeyBuilder const & value_builder() const { return m_value_builder; }
-
-    gsl::span<std::int32_t const> get_typed_offset_data() const
-    {
-        return m_offset_values.get_data_span();
-    }
-
-private:
-    ExpandableBuffer<std::int32_t> m_offset_values;
-    StringDictionaryKeyBuilder m_key_builder;
-    StringDictionaryKeyBuilder m_value_builder;
-};
-
-template <std::size_t CurrentIndex, typename BuilderTuple, typename Arg>
-arrow::Result<std::size_t> unpack_struct_builder_args(BuilderTuple & builders, Arg && arg)
-{
-    auto & builder = std::get<CurrentIndex>(builders);
-    auto index = builder.length();
-    ARROW_RETURN_NOT_OK(builder.append(arg));
-    return index;
-}
-
-template <std::size_t CurrentIndex, typename BuilderTuple, typename FirstArg, typename... Args>
-arrow::Result<std::size_t>
-unpack_struct_builder_args(BuilderTuple & builder, FirstArg && first_arg, Args &&... args)
-{
-    ARROW_RETURN_NOT_OK(unpack_struct_builder_args<CurrentIndex>(builder, first_arg));
-    return unpack_struct_builder_args<CurrentIndex + 1>(builder, std::forward<Args &&>(args)...);
-}
-
 }  // namespace detail
-
-template <typename... BuilderTypes>
-class StructBuilder {
-public:
-    StructBuilder(arrow::MemoryPool * pool)
-    {
-        detail::for_each_in_tuple(
-            m_builders, [&](auto & x, std::size_t _) { (void)x.init_buffer(pool); });
-    }
-
-    template <typename... Args>
-    arrow::Result<std::size_t> append(Args &&... args)
-    {
-        return detail::unpack_struct_builder_args<0>(m_builders, std::forward<Args &&>(args)...);
-    }
-
-    std::tuple<BuilderTypes...> & builders() { return m_builders; }
-
-private:
-    std::tuple<BuilderTypes...> m_builders;
-};
 
 class POD5_FORMAT_EXPORT PoreWriter : public DictionaryWriter {
 public:
