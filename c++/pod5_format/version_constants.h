f38b4c3d1d4186209505a5d348f9e6e5a94fd9fe
Richard Harris <richard.harris@nanoporetech.com>
2026-08-24 12:21:03 +0000

    Read table v5 version

--- c++/pod5_format/version_constants.h
+++ c++/pod5_format/version_constants.h
@@ -15,7 +15,7 @@ inline Version const kPod5VersionReadTableV3Written{0, 0, 35};
 inline Version const kPod5VersionReadTableV3MigrationThreshold{0, 0, 38};
 
 inline Version const kPod5VersionReadTableV4{0, 3, 30};
-inline Version const kPod5VersionReadTableV5{0, 3, 40};
+inline Version const kPod5VersionReadTableV5{0, 3, 44};
 inline Version const kPod5VersionReadTableV6{0, 3, 46};
 
 inline Version const kPod5VersionReadTableLatest = kPod5VersionReadTableV6;
